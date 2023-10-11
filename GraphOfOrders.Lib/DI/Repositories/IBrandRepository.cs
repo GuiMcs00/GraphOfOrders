860d22946ec000764fc971282d2541d3d363d38a
Danrley Pereira <danrleywillian@gmail.com>
2023-10-10 22:48:09 -0300

    Brand paginated and by Id

--- GraphOfOrders.Lib/DI/Repositories/IBrandRepository.cs
+++ GraphOfOrders.Lib/DI/Repositories/IBrandRepository.cs
@@ -1,11 +1,14 @@
 using GraphOfOrders.Lib.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace GraphOfOrders.Lib.DI
 {
     public interface IBrandRepository
     {
         IEnumerable<Brand> GetBrandsByProduct(int productId);
+        IEnumerable<Brand> GetBrands(int howMany, int page);
+        Task<Brand> GetBrandById(int brandId);
     }
 
 }
