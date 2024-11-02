c1e3f06ca79e7937bec7891f317b635265ff93d9
Danrley Pereira <danrleywillyan@outlook.com>
2024-11-02 18:27:53 -0300

    Feature/change version and github workflow (#1)
    Implementing authentication and changing from .net 7.0 to .net 8.0 .TODO: Check the DbContext type and run migrations
    ---------
    Co-authored-by: Polyana Cunha <polyanacunha10@gmail.com>

--- GraphOfOrders.Api/Program.cs
+++ GraphOfOrders.Api/Program.cs
@@ -1,8 +1,18 @@
 using GraphOfOrders.Repo.IoC;
 using GraphOfOrders.Service.IoC;
 using GraphOfOrders.Service;
+using Microsoft.AspNetCore.Identity;
+using GraphOfOrders.Repo;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+//check this line
+builder.Services.AddDbContext<OrdersContext>();
+builder.Services.AddIdentityCore<MyUser>()
+    .AddEntityFrameworkStores<OrdersContext>()
+    .AddApiEndpoints();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 
@@ -33,7 +43,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapIdentityApi<MyUser>();
+app.MapGet("/", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}")
+    .RequireAuthorization();
 app.Run();
 
 // Make the implicit Program class public so test projects can access it
 public partial class Program { }
+
+class MyUser : IdentityUser { }
